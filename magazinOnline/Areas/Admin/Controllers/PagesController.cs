4e960ccd136af04a206d3d455a5ad49f4a0577ce
Mihai Aurelian <aurasslam@gmail.com>
2019-01-06 18:38:28 +0200

    Adaugare Pagini

---
+++ magazinOnline/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,33 @@
+﻿using magazinOnline.Models.Data;
+using magazinOnline.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace magazinOnline.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            //Lista de PageVM
+            List<PageVM> pagesList;
+            using (Db db = new Db())
+            {
+                //Initializarea listei
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+
+            }
+            //returneaza View-ul
+                return View(pagesList);
+        }
+        // GET: Admin/Pages/AddPage
+        public ActionResult AddPage()
+        {
+            return View();
+        }
+    }
+}
