28cc38d228a7e97e6120c86d94503716415ec342
Mihai Aurelian <aurasslam@gmail.com>
2019-01-08 00:32:26 +0200

    adaugare produse + poze

--- magazinOnline/Models/Data/Db.cs
+++ magazinOnline/Models/Data/Db.cs
@@ -12,8 +12,8 @@ namespace magazinOnline.Models.Data
         public DbSet<PageDTO> Pages { get; set; }
         public DbSet<SidebarDTO> Sidebar { get; set; }
         public DbSet<CategoryDTO> Categories { get; set; }
-       /* public DbSet<ProductDTO> Products { get; set; }
-        public DbSet<UserDTO> Users { get; set; }
+        public DbSet<ProductDTO> Products { get; set; }
+       /* public DbSet<UserDTO> Users { get; set; }
         public DbSet<RoleDTO> Roles { get; set; }
         public DbSet<UserRoleDTO> UserRoles { get; set; }
         public DbSet<OrderDTO> Orders { get; set; }
